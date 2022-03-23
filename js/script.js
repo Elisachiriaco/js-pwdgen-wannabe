463314c4747ede7c6c852209575b84a42ed31f04
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-03-23 16:42:25 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// MermaidJS
+
+// graph TD
+// I[Mi chiedo se usare const o let per definire la variabile]--> A 
+// A[Chiedo il nome] --> B[Chiedo il cognome]
+// B --> C[Chiedo il colore preferito]
+// C --> D[Verifico che sia tutto giusto tramite l'inspector]
+// D -->|giusto| E[Procedo stampando sulla pagina nome,cognome,colore preferito e 21]
+// D -->|sbagliato| F[controllo quale può essere il problema]
+// F --> G[risolvo il problema]
+// G --> E 
+// E --> H[Fine]  
+
+
+
 let userName = prompt('Come ti chiami?');
 console.log(userName);
 
